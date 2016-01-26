886c26ac02f3d4db708f3e030e0212a105263ba4
ide <ide_bradley@hotmail.com>
2016-01-26 11:51:46 +0000

    First commit

---
+++ TaskListApp/ViewModels/TaskListIndexData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using TaskListApp.Models;
+using System.Linq;
+using System.Web;
+
+namespace TaskListApp.ViewModels
+{
+    public class TaskListIndexData
+    {     
+            public IEnumerable<TaskList> TaskLists{ get; set; }
+            public IEnumerable<Task> Tasks { get; set; }
+            
+        }
+    }
+
+
+
